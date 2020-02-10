9e4f1054c575d47d40d06e8311ab3b7f7f2b0eda
baretabel <49393886+baretabel@users.noreply.github.com>
2020-02-10 11:01:32 +0400

    Add files via upload

---
+++ resources/views/serpe/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+<div class="form-cad" style="margin-top: 5%; text-align: center;" >
+<h2>Erreur 403</h2>
+<br><br>
+<p>Accès refuser</p><br>
+
+<div>
+@endsection
+
