905a2f250d188a98b71107870298b07bdf254f72
baretabel <49393886+baretabel@users.noreply.github.com>
2020-02-11 19:32:49 +0400

    Add files via upload

--- resources/views/serpe/article.blade.php
+++ resources/views/serpe/article.blade.php
@@ -5,7 +5,8 @@
 <h3>{{ $article->titre }}</h3>
 <br><br>
 <p>{{ $article->post }}</p><br>
-<p style="font-size:0.7em"> crée le {{ $article->created_at }} par {{ $article->user->pseudo }}</p>
+<p style="font-size:0.7em"> crée le {{ $article->created_at }} par {{ $article->user->pseudo }}</p><br>
+<a href="/espece/{{ $article->espece_id }}" class="btn btn-success form-control">Retour</a>
 <div>
 @endsection
 
