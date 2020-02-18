26e12ff15b1b10621208f7d61ae3881fd89164d0
baretabel <49393886+baretabel@users.noreply.github.com>
2020-02-18 15:17:40 +0400

    Add files via upload

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -1,10 +1,10 @@
 <nav class="nav flex-column bg-danger" style="width: 170px;height:100%;margin: 0px !important;">
   <ul>
-    <li><a href="/especes">Especes</a></li>
-    <li><a href="/articles">Articles</a></li>
-    <li><a href="/commentaire">Commentaires</a></li>
+    <li  style="  margin-top: 25px;"><a href="/especes">Especes</a></li>
+    <li style="  margin-top: 25px;"><a href="/articles" >Articles</a></li>
+    <li style="  margin-top: 25px;"><a href="/commentaire" >Commentaires</a></li>
     @if(Auth::user()->role_id==3)
-    <li><a href="/user">Utilisateurs</a></li>
+    <li style="  margin-top: 25px;"><a href="/user"style="  margin-top: 25px;">Utilisateurs</a></li>
     @endif
 </ul>
 </nav>
